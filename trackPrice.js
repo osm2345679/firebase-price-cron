9ac8327aa853520c8dfff458fb37ccb4460960ea
osm2345679 <57944996+osm2345679@users.noreply.github.com>
2025-06-08 13:15:41 +0900

    Update trackPrice.js

--- trackPrice.js
+++ trackPrice.js
@@ -1,5 +1,5 @@
 const admin = require('firebase-admin');
-const fetch = require('node-fetch');
+const fetch = (...args) => import('node-fetch').then(({default: fetch}) => fetch(...args));
 
 const serviceAccount = JSON.parse(process.env.GOOGLE_APPLICATION_CREDENTIALS_JSON);
 
