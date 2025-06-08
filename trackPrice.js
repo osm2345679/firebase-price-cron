ea8fe79679e9b2ede48b45be2f6ffe8bbb81bc30
osm2345679 <57944996+osm2345679@users.noreply.github.com>
2025-06-08 13:07:13 +0900

    Update trackPrice.js

--- trackPrice.js
+++ trackPrice.js
@@ -1,12 +1,10 @@
 const admin = require('firebase-admin');
 const fetch = require('node-fetch');
 
+const serviceAccount = JSON.parse(process.env.GOOGLE_APPLICATION_CREDENTIALS_JSON);
+
 admin.initializeApp({
-  credential: admin.credential.cert({
-    projectId: process.env.FIREBASE_PROJECT_ID,
-    clientEmail: process.env.FIREBASE_CLIENT_EMAIL,
-    privateKey: process.env.FIREBASE_PRIVATE_KEY.replace(/\\n/g, '\n'),
-  }),
+  credential: admin.credential.cert(serviceAccount),
 });
 
 function makeFirestoreSafe(str) {
